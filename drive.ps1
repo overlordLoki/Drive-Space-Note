e7bad1101b24ec5b495c1c23eb32bc5e99a91bca
Loki <101067851+overlordLoki@users.noreply.github.com>
2022-11-29 09:32:33 +1300

    more done

--- drive.ps1
+++ drive.ps1
@@ -17,12 +17,36 @@ try {
     }
 }
 catch {
-    {1:<#Do this if a terminating exception happens#>}
+    throw
 }
 
-# verify if the log directory exists
+Add-Content -Path $LogFile -Value "[INFO] Running $PSCommandPath"
 # verify that PoshGram is installed
+if(-NOT (Get-Module -Name PoshGram -ListAvailable)){
+    Add-Content -Path $LogFile -Value "[ERROR] PoshGram is not installed. Please install PoshGram"
+    throw
+}else{
+    Add-Content -Path $LogFile -Value "[INFO] PoshGram is installed"
+}
 # get the hard drive information
+if ($PSVersionTable.Platform -eq "Win32NT") {
+    # sys is windows
+    # used
+    # free
+}else {
+    # sys is linux
+    # used
+    # free
+    $volume = Get-PSDrive -Name $Drive
+    #verify that the volume is found
+    if ($volume -eq $null) {
+        Add-Content -Path $LogFile -Value "[ERROR] $Drive is not found"
+        throw
+    }
+    $total = $volume.Used + $volume.Free
+    
+
+}
 # send telegram message
 
 $botToken = 'nnnnnnnnn:xxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxx'
